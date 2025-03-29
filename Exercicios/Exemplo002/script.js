860348005e168f8ed71e271120699733321a4baa
João Fellipe <163138415+joaofelliperomao@users.noreply.github.com>
2025-03-29 11:14:40 -0300

    Add files via upload

---
+++ Exercicios/Exemplo002/script.js
@@ -0,0 +1,3 @@
+var meuElemento = document.getElementById("paragrafo")
+console.log(meuElemento)
+console.log(meuElemento.textContent)
---
+++ Exercicios/Exemplo004/script.js
@@ -0,0 +1,11 @@
+var B1 = document.getElementById("B1") 
+
+B1.onclick = function()
+{
+    alert("Voce clicou")
+    botao1.textContent = "Clicado"
+    print("Numero de cliks")
+}
+
+B2 = document.getElementById("B2")
+    
